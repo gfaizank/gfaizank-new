36088b1d323e925e29a4b12fcb52457ad0eddcbd
gfaizank <faizan2017.fk@gmail.com>
2024-03-04 01:40:10 +0530

    initial commit

---
+++ src/components/HomePage/About.js
@@ -0,0 +1,97 @@
+import Skills from './Skills';
+import Button from '../UI/Button';
+
+const About = () => {
+  return (
+    <section id="about" className="pt-16 pb-16 lg:pt-20">
+      <div className="px-2 mb-12 text-center lg:mb-16">
+        <h2>About Me</h2>
+        <p className="max-w-2xl px-4 mx-auto text-lg text-gray-800 sm:px-10 md:text-xl md:max-w-3xl md:px-0">
+          Here you will find more information about me, what I do, and my
+          current skills in terms of programming and technology.
+        </p>
+      </div>
+
+      <div className="container mx-auto max-w-7xl flex flex-col space-y-12 px-4 sm:px-10 lg:flex-row lg:space-y-0 lg:space-x-16">
+        <div className="w-full">
+          <h3 className="mb-6">Get to know me!</h3>
+          <div className="flex flex-col items-center space-y-4 mb-8 text-lg text-gray-800 lg:mb-10">
+            <p>
+              I'm a{' '}
+              <span className="font-semibold">Software Engineer</span> with
+              a passion for building up beautiful and responsive websites. I
+              have worked on many projects that demonstrates my ability to make
+              use of the concepts I've learned. You can check them out in the{' '}
+              <span className="font-semibold">Projects</span> section.
+            </p>
+            <p>
+              I enjoy sharing and interacting with people in the Web Dev world.
+              I believe that this helps me to grow and learn in this field. You
+              can connect with me on{' '}
+              <span>
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="www.linkedin.com/in/faizan-khan-8b622014b"
+                  className="inline-block bg-clip-text text-transparent bg-gradient-to-r from-orange-400 to-orange-600 font-bold"
+                >
+                  LinkedIn
+                </a>
+              </span>{' '}
+              and{' '}
+              <span>
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://x.com/Mr_faizan1818?t=iVd3O2QgLuIY-AU0FWLC2g&s=08"
+                  className="inline-block bg-clip-text text-transparent bg-gradient-to-r from-orange-400 to-orange-600 font-bold"
+                >
+                  Twitter
+                </a>
+              </span>{' '}
+              where I share my experiences and thoughts on various topics
+              related to Web Development.
+            </p>
+            <p>
+              I'm open to <span className="font-semibold">Job</span>{' '}
+              opportunities where I can learn, share ideas and work with
+              like-minded people. If you have any good opportunities that matches
+              my skills and experience, please don't hesitate to {''}
+              <span className="font-semibold">contact</span> me.
+            </p>
+          </div>
+          <Button link="mailto:faizan2017.fk@gmail.com" text="Contact" />
+        </div>
+
+        <div className="w-full">
+          <h3 className="mb-6">My Skills</h3>
+          <div className="flex flex-col space-y-4">
+            <div className="flex space-x-4">
+              <Skills text="HTML" />
+              <Skills text="CSS" />
+              <Skills text="SASS" />
+              <Skills text="JavaScript" />
+            </div>
+            <div className="flex space-x-4">
+              <Skills text="TypeScript" />
+              <Skills text="React" />
+              <Skills text="Git" />
+              <Skills text="GitHub" />
+            </div>
+            <div className="flex space-x-4">
+              <Skills text="Python" />
+              <Skills text="Django" />
+              <Skills text="Flask" />
+            </div>
+            <div className="flex space-x-4">
+              <Skills text="Tailwind CSS" />
+              <Skills text="Responsive Design" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/HomePage/Footer.js
@@ -0,0 +1,65 @@
+import { BsLinkedin, BsTwitter, BsGithub } from 'react-icons/bs';
+
+const Footer = () => {
+  return (
+    <footer className="container mx-auto max-w-7xl pt-12 pb-8 px-4 text-gray-800 lg:pt-16 md:px-10 lg-pt-20">
+      <div className="flex flex-col-reverse items-start justify-between space-y-reverse space-y-12 mb-10 sm:flex-row sm:space-y-0">
+        <div>
+          <h4 className="mb-4 text-2xl font-bold uppercase">Faizan Khan</h4>
+          <p className="max-w-lg">
+            A Software Engineer focused on building the frontend and backend of
+            websites that leads to the success of the overall product
+          </p>
+        </div>
+
+        <div className="flex flex-col items-start md:items-center">
+          <h4 className="mb-4 text-2xl font-bold uppercase">Social</h4>
+          <div className="flex space-x-4">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="www.linkedin.com/in/faizan-khan-8b622014b"
+              className="inline-block transition-all hover:scale-110"
+            >
+              <BsLinkedin size={24} />
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://x.com/Mr_faizan1818?t=iVd3O2QgLuIY-AU0FWLC2g&s=08"
+              className="inline-block transition-all hover:scale-110"
+            >
+              <BsTwitter size={24} />
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://github.com/gfaizank"
+              className="inline-block transition-all hover:scale-110"
+            >
+              <BsGithub size={24} />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="mt-8 text-center text-sm">
+        <p>
+          &copy; Copyright 2024. Made by{' '}
+          <span>
+            <a
+              href="http://gfaizank.netlify.app/"
+              className="inlne-block font-bold underline"
+            >
+                Faizan Khan
+            </a>
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
